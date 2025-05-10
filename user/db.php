22db18b7c3752a8c7c15290340e6051ec770ac05
Junessa19 <bahianjunessa@hmail.com>
2025-05-10 19:50:21 +0800

    added db

---
+++ user/db.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'config.php';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
