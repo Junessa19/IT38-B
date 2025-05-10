6a6574a8ba8af380f7446b2e001228aad0e4657a
Junessa19 <bahianjunessa@hmail.com>
2025-05-10 23:16:54 +0800

    Added the changes

--- user/config.php
+++ user/config.php
@@ -1,6 +1,14 @@
 <?php
-$host = 'localhost';
-$db   = 'inventory';
-$user = 'root';
-$pass = ''; 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "inventory";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 ?>
